610b570e89741a6c531c168d47257b8d8559ff52
agraham <agraham0925@gmail.com>
2018-03-27 14:21:27 -0500

    created files and T class

---
+++ js/app.js
@@ -0,0 +1,32 @@
+console.log('test test')
+
+// Create a class for you tomagotchi - COMMIT
+class myTomagotchi {
+	constructor(name){
+		name = name;
+	}
+	greet(user) {
+		console.log("hi " + user)
+	}
+}
+
+// Display a character of your choice on the screen to represent your pet  - COMMIT
+	//temporarily make a shape?
+
+// Display the following metrics for your pet. Hunger (1-10 scale), Sleepiness (1-10 scale), Boredom (1-10 scale), Age  - COMMIT
+
+// Add buttons to the screen to feed your pet, turn off the lights, and play with your pet  - COMMIT
+
+// Add the ability to name your pet  - COMMIT
+
+// Style the page  - COMMIT
+
+// Increase your pet's age every x minutes  - COMMIT
+
+// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.  - COMMIT
+
+// You pet should die if Hunger, Boredom, or Sleepiness hits 10.  - COMMIT
+
+// Morph your pet at certain ages  - COMMIT
+
+// Animate your pet across the screen  - COMMIT
