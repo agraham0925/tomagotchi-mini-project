aeced1d69411b4725e8e1c25cba5aefd2fae247d
agraham <agraham0925@gmail.com>
2018-03-28 12:21:43 -0500

    got hunger, sleepy and bored functions working

--- js/app.js
+++ js/app.js
@@ -1,21 +1,22 @@
 console.log('test test')
 let userTomagotchi;
 
-// Create a class for you tomagotchi - COMMIT
+// Create a class for you tomagotchi
 class tomagotchiTemplate {
 	constructor(name){
 		this.hunger = 5;
-		this.sleepiness = 4;
-		this.boredom = 4;
+		this.sleepiness = 5;
+		this.boredom = 5;
 		this.age = 0;
 		this.name = name;
 	}
 	greet(user) {
-		console.log("hi " + user)
+		console.log("hi " + user);
 	}
 }
 
-// Add the ability to name your pet  - COMMIT
+// Add the ability to name your pet
+// This also begins your pet's "life" and calls the hunger, sleepy and bored functions
 $('#submit').on('click', (event) => {
 
 	//show name on screen
@@ -25,6 +26,9 @@ $('#submit').on('click', (event) => {
 	userTomagotchi = new tomagotchiTemplate(newName);
 
 	$('#name').text("Your Tomagochi's name is " + newName)
+	// petHunger();
+	// petSleepy();
+	// petBored();
 })
 
 
@@ -41,27 +45,68 @@ $('#feedbutton').on('click', (event) => {
 })
 
 //Light on/off button
-$('#lightsbutton').on('click', (event) => {
-	//if lights on equals true, turn lights off
-	if()
-	//else lights on
-
 
 
 //Play button
 $('#playbutton').on('click', (event) => {
 
-	//if hunger is greater than 0, run code
+	//if boredom is greater than 0, run code
 	let play = userTomagotchi.boredom
+
 	if(play > 0) {
 		userTomagotchi.boredom = userTomagotchi.boredom - 1
 	}
 	console.log(userTomagotchi.boredom)
 })
 
-// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.  - COMMIT
+// Increase your pet's Hunger on an interval of your choosing.
+
+const petHunger = () => {
+	const timer = setInterval ( () => {
+		let hunger = userTomagotchi.hunger
+
+		if(hunger < 10){
+			userTomagotchi.hunger = userTomagotchi.hunger + 1;
+			console.log("hunger level: " + userTomagotchi.hunger);
+		} else {
+		clearInterval(timer)
+		console.log("your pet has died of starvation!");
+	}
+	}, 2000);
+}
 
 
+// Increase your pet's Sleepiness on an interval of your choosing.
+
+const petSleepy = () => {
+	const timer = setInterval ( () => {
+		let tired = userTomagotchi.sleepiness
+
+		if(tired < 10){
+			userTomagotchi.sleepiness = userTomagotchi.sleepiness + 1;
+			console.log("sleepiness level: " + userTomagotchi.sleepiness);
+		} else {
+		clearInterval(timer)
+		console.log("your pet has died of sleep deprivation!");
+	}
+	}, 2000);
+}
+
+// Increase your pet's Bored metrics on an interval of your choosing.
+
+const petBored = () => {
+	const timer = setInterval ( () => {
+		let boredom = userTomagotchi.boredom
+
+		if(boredom < 10){
+			userTomagotchi.boredom = userTomagotchi.boredom + 1;
+			console.log("boredom level: " + userTomagotchi.boredom);
+		} else {
+		clearInterval(timer)
+		console.log("your pet has died of boredom!");
+	}
+	}, 2000);
+}
 
 // Increase your pet's age every x minutes  - COMMIT
 
